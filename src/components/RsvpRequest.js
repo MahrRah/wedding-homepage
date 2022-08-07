81a91d489107c7b527478a883d07335666326a2a
Tamara Rahimi <mahra.rah@gmail.com>
2022-08-07 09:32:36 +0200

    Fix  readonly and plusOne section

--- src/components/RsvpRequest.js
+++ src/components/RsvpRequest.js
@@ -10,11 +10,11 @@ class RsvpRequest extends Component {
             submitted: false,
             firstname: '',
             lastname: '',
-            attending: false,
+            attending: "yes",
             dinner: '',
             allowedBrunch: false,
             brunch: false,
-            plusOne: null,
+            plusOne: { name: "", lastname: "", food: "" },
             hasPlusOne: false,
             bringsPlusOne: "",
             children: undefined,
@@ -27,6 +27,7 @@ class RsvpRequest extends Component {
         this.handleSubmitCode = this.handleSubmitCode.bind(this);
         this.isSubmitted = this.isSubmitted.bind(this);
         this.changeStateAPI = this.changeStateAPI.bind(this);
+        this.onChangePlusOne = this.onChangePlusOne.bind(this);
     }
 
     handleSubmitCode = async (e) => {
@@ -67,10 +68,11 @@ class RsvpRequest extends Component {
     };
 
     changeStateAPI(data) {
-        this.setState({ lastname: data.lastname, firstname: data.name, brunch: data.brunch, allowedBrunch: data.allowedBrunch, dinner: data.dinner })
+        this.setState({ lastname: data.lastname, firstname: data.name, brunch: data.brunch, allowedBrunch: data.allowedBrunch, dinner: data.food })
 
         if (data.plusOne.length > 0) {
-            this.setState({ hasPlusOne: true, bringsPlusOne: "yes", plusOne: data.plusOne });
+
+            this.setState({ hasPlusOne: true, bringsPlusOne: "yes", plusOne: data.plusOne[0] });
         }
         if (data.child.length > 0) {
             this.setState({ hasChildren: true, bringsChildren: true, children: data.child })
@@ -79,17 +81,20 @@ class RsvpRequest extends Component {
     }
 
     onChange(e) {
-        console.log(e)
         this.setState({ [e.target.name]: e.target.value });  // Getting access to entered values
     }
-    
+    onChangePlusOne(e) {
+        let state = this.state.plusOne
+        state[e.target.name] = e.target.value
+        this.setState({ plusOne: state });
+    }
     isSubmitted = () => {
         this.setState({ submitted: true })
     }
 
     render() {
         return (
-            <><div id="main">
+            <div id="main">
                 <div>
                     <h2>RSVP</h2>
                     <p>asdsd</p>
@@ -107,61 +112,109 @@ class RsvpRequest extends Component {
                             <li><input type="submit" value={this.props.t('send')} /></li>
                         </ul>
                     </form>}
-            </div><div id="main">
-                    {this.state.submitted &&
-                        <form method="post" action="#">
-                            <div className="col-4 col-12-small">
-                                            <input type="radio" id="attending-true" name="attending" value="yes" checked={this.state.attending  ==="yes"} onChange={this.onChange}/>
-                                            <label for="bringsPlusOne-true">Attending</label>
-                                        </div>
-                                        <div className="col-4 col-12-small">
-                                            <input type="radio" id="attending-false" name="attending" value="no" checked={this.state.attending  ==="no"} onChange={this.onChange}/>
-                                            <label for="attending-false">Not attending</label>
-                                        </div>
+                {this.state.submitted &&
+                    <form method="post" action="#">
+                        <div className="col-4 col-12-small">
+                            <input type="radio" id="attending-true" name="attending" value="yes" checked={this.state.attending == "yes"} onChange={this.onChange} />
+                            <label for="bringsPlusOne-true">Attending</label>
+                        </div>
+                        <div className="col-4 col-12-small">
+                            <input type="radio" id="attending-false" name="attending" value="no" checked={this.state.attending == "no"} onChange={this.onChange} />
+                            <label for="attending-false">Not attending</label>
+                        </div>
+                        <div className="row gtr-uniform">
+                            <div className="col-6 col-12-xsmall">
+                                <input readonly type="text" name="firstname" id="firstname" placeholder="First Name" value={this.state.firstname} />
+                            </div>
+                            <div className="col-6 col-12-xsmall">
+                                <input readonly type="text" name="lastname" id="lastname" placeholder="Last Name" value={this.state.lastname} />
+                            </div>
+                            <div className="col-12">
+                                <select name="dinner" id="dinner" value={this.state.dinner} onChange={this.onChange}>
+                                    <option value="0">- Meal options -</option>
+                                    <option value="1">Vegarian</option>
+                                    <option value="2">Vegan</option>
+                                    <option value="3">Gluten free</option>
+                                    <option value="4">No Dietary restirctions</option>
+                                </select>
+                            </div>
+                        </div>
+                        <hr />
+
+                        {this.state.hasPlusOne &&
                             <div className="row gtr-uniform">
-                                <div className="col-6 col-12-xsmall">
-                                    <input type="text" name="firstname" id="firstname" placeholder="First Name" value={this.state.firstname} onChange={this.onChange} />
+                                <div className="col-4 col-12-small">
+                                    <input type="radio" id="bringsPlusOne-true" name="bringsPlusOne" value="yes" checked={this.state.bringsPlusOne === "yes"} onChange={this.onChange} />
+                                    <label for="bringsPlusOne-true">with Plus One</label>
                                 </div>
-                                <div className="col-6 col-12-xsmall">
-                                    <input type="text" name="lastname" id="lastname" placeholder="Last Name" value={this.state.lastname} onChange={this.onChange} />
+                                <div className="col-4 col-12-small">
+                                    <input type="radio" id="bringsPlusOne-false" name="bringsPlusOne" value="no" checked={this.state.bringsPlusOne === "no"} onChange={this.onChange} />
+                                    <label for="bringsPlusOne-false">without Plus One</label>
                                 </div>
-                                <div className="col-12">
-                                    <select name="dinner" id="dinner" value={this.state.dinner} onChange={this.onChange}>
-                                        <option value="0">- Meal options -</option>
-                                        <option value="1">Vegarian</option>
-                                        <option value="2">Vegan</option>
-                                        <option value="3">Gluten free</option>
-                                        <option value="4">No Dietary restirctions</option>
-                                    </select>
-                                </div>
+                                {this.state.bringsPlusOne === "yes" &&
+                                    <>
+                                        <div className="col-6 col-12-xsmall">
+                                            <input type="text" name="name" id="firstname-plusone" placeholder="Plus One Firstname" value={this.state.plusOne.name} onChange={this.onChangePlusOne} />
+                                        </div>
+                                        <div className="col-6 col-12-xsmall">
+                                            <input type="text" name="lastname" id="firstname-plusone" placeholder="Plus One Lastname" value={this.state.plusOne.lastname} onChange={this.onChangePlusOne} />
+                                        </div>
+                                        <div className="col-12">
+                                            <select name="dinner" id="food" value={this.state.plusOne.dinner} onChange={this.onChangePlusOne}>
+                                                <option value="0">- Meal options -</option>
+                                                <option value="1">Vegarian</option>
+                                                <option value="2">Vegan</option>
+                                                <option value="3">Gluten free</option>
+                                                <option value="4">No Dietary restirctions</option>
+                                            </select>
+                                        </div></>
+                                }
+                                <hr />
+                            </div>
+                        }
+                        <div className="col-12">
+                            <textarea name="demo-message" id="demo-message" placeholder="Additional Notes" rows="3"></textarea>
+                        </div><div className="col-12">
+                            <ul className="actions">
+                                <li><input type="submit" value="Send Message" className="primary" /></li>
+                                <li><input type="reset" value="Reset" /></li>
+                            </ul>
+                        </div>
+                    </form>
+                }
+            </div>);
+
+
+        {/* <div id="main">
+          
                                 <hr />
                                 {this.state.hasPlusOne &&
-                                    <>
+                                    <div className="row gtr-uniform">
                                         <div className="col-4 col-12-small">
-                                            <input type="radio" id="bringsPlusOne-true" name="bringsPlusOne" value="yes" checked={this.state.bringsPlusOne  ==="yes"} onChange={this.onChange}/>
+                                            <input type="radio" id="bringsPlusOne-true" name="bringsPlusOne" value="yes" checked={this.state.bringsPlusOne === "yes"} onChange={this.onChange} />
                                             <label for="bringsPlusOne-true">with Plus One</label>
                                         </div>
                                         <div className="col-4 col-12-small">
-                                            <input type="radio" id="bringsPlusOne-false" name="bringsPlusOne" value="no" checked={this.state.bringsPlusOne  ==="no"} onChange={this.onChange}/>
+                                            <input type="radio" id="bringsPlusOne-false" name="bringsPlusOne" value="no" checked={this.state.bringsPlusOne === "no"} onChange={this.onChange} />
                                             <label for="bringsPlusOne-false">without Plus One</label>
                                         </div>
-                                        {this.state.bringsPlusOne==="yes" &&
-                                        <><div className="col-6 col-12-xsmall">
-                                            <input type="text" name="firstname-plusone" id="firstnameplusone" placeholder="First Name Plus One" value={this.state.plusOne[0].name} onChange={this.onChange} />
-                                        </div><div className="col-6 col-12-xsmall">
-                                                <input type="text" name="lastnameplusone" id="lastnameplusone" placeholder="Last Name Plus One" value={this.state.plusOne[0].lastname} onChange={this.onChange} />
-                                            </div><div className="col-12">
-                                                <select name="dinner" id="dinner" value={this.state.plusOne.dinner} onChange={this.onChange}>
-                                                    <option value="0">- Meal options -</option>
-                                                    <option value="1">Vegarian</option>
-                                                    <option value="2">Vegan</option>
-                                                    <option value="3">Gluten free</option>
-                                                    <option value="4">No Dietary restirctions</option>
-                                                </select>
-                                            </div></>
+                                        {this.state.bringsPlusOne === "yes" &&
+                                            <><div className="col-6 col-12-xsmall">
+                                                <input type="text" name="firstname-plusone" id="firstname-plusone" placeholder=" Plus One Firstname" value={this.state.plusOne[0].name} onChange={this.onChange} />
+                                            </div><div className="col-6 col-12-xsmall">
+                                                    <input type="text" name="lastname-plusone" id="firstname-plusone" placeholder=" Plus One Lastname" value={this.state.plusOne[0].lastname} onChange={this.onChange} />
+                                                </div><div className="col-12">
+                                                    <select name="dinner" id="dinner" value={this.state.plusOne.dinner} onChange={this.onChange}>
+                                                        <option value="0">- Meal options -</option>
+                                                        <option value="1">Vegarian</option>
+                                                        <option value="2">Vegan</option>
+                                                        <option value="3">Gluten free</option>
+                                                        <option value="4">No Dietary restirctions</option>
+                                                    </select>
+                                                </div></>
                                         }
                                         <hr />
-                                    </>
+                                    </div>
                                 }
                                 {this.state.hasChildren &&
                                     <>
@@ -204,19 +257,12 @@ class RsvpRequest extends Component {
                                             <label for="brunch-no">No</label>
                                         </div>
                                     </div>}
-                                <div className="col-12">
-                                    <textarea name="demo-message" id="demo-message" placeholder="Additional Notes" rows="3"></textarea>
-                                </div>
-                                <div className="col-12">
-                                    <ul className="actions">
-                                        <li><input type="submit" value="Send Message" className="primary" /></li>
-                                        <li><input type="reset" value="Reset" /></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </form>}
-                </div></>
-        );
+                                
+
+                        </form>
+                        }
+                        </div> */}
+
     }
 }
 
