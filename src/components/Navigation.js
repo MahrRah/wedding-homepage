2e56bb2b0aab53191772dddfe6e5199f5308d0b5
Tamara Rahimi <mahra.rah@gmail.com>
2022-08-27 22:46:09 +0200

    Add menu for small screens

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -11,31 +11,29 @@ import { useTranslation } from "react-i18next";
 class Navigation extends Component {
   constructor(props) {
     super(props);
+    this.state = { menuOn: false }
     this.changeLocation = this.changeLocation.bind(this);
+    this.showNavbarMenu = this.showNavbarMenu.bind(this);
+    this.closeNavbarMenu = this.closeNavbarMenu.bind(this);
   }
   changeLocation = (route) => {
     return this.props.location.pathname === route ? "active" : null
-
-  }
-
-  useEffect = () => {
-    const script = document.createElement('script');
-    script.src = "../assets/js/main.js";
-    script.async = true;
-    script.type = "text/jsx"
-    document.body.appendChild(script);
-    return () => {
-      document.body.removeChild(script);
-    }
   }
 
   changeLanguage = (lng) => {
     i18n.changeLanguage(lng);
   }
 
+  showNavbarMenu = () => {
+    this.setState({ menuOn: true });
+  }
+  closeNavbarMenu = () => {
+    this.setState({ menuOn: false });
+  }
+
   render() {
     return (
-      <nav id="nav">
+      <><nav id="nav">
         <ul className="links">
           <li className={this.changeLocation("/")}> <Link to="/">{this.props.t('common:overview')}</Link></li>
           <li className={this.changeLocation("/details")}> <Link to="/details">{this.props.t('common:details')}</Link></li>
@@ -48,6 +46,28 @@ class Navigation extends Component {
           <li><a onClick={() => this.changeLanguage('de')}><span className="label">DE</span></a></li>
         </ul>
       </nav>
+        <a id='navPanelToggle' onClick={this.showNavbarMenu} >Menu</a>
+        {this.state.menuOn &&
+          <body class="is-navPanel-visible">
+            <div id="navPanel">
+              <nav>
+                <ul className="links">
+                  <li className={this.changeLocation("/")}> <Link to="/">{this.props.t('common:overview')}</Link></li>
+                  <li className={this.changeLocation("/details")}> <Link to="/details">{this.props.t('common:details')}</Link></li>
+                  <li className={this.changeLocation("/location")}> <Link to="/location">{this.props.t('common:location')}</Link></li>
+                  <li className={this.changeLocation("/rsvp")}> <Link to="/rsvp">{this.props.t('common:rsvp')}</Link></li>
+                  <li className={this.changeLocation("/gallery")}> <Link to="/gallery">{this.props.t('common:gallery')}</Link></li>
+                </ul>
+                <ul className="icons">
+                  <li><a onClick={() => this.changeLanguage('en')}><span className="label">EN</span></a></li>
+                  <li><a onClick={() => this.changeLanguage('de')}><span className="label">DE</span></a></li>
+                </ul>
+              </nav>
+              <a href="#navPanel" class="close" onClick={this.closeNavbarMenu} />
+            </div>
+          </body>
+        }
+      </>
 
 
     );
