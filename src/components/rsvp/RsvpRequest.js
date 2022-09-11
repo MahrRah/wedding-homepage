9899d6ffc8d7c8d97d4b8d08e66aa43383bbb167
MahrRah <mahra.rah@gmail.com>
2022-09-11 21:42:31 +0200

    fix typi

--- src/components/rsvp/RsvpRequest.js
+++ src/components/rsvp/RsvpRequest.js
@@ -305,7 +305,7 @@ class RsvpRequest extends Component {
             }
             console.log(this.state.rsvpCode)
 
-            const res = await fetch(` http://localhost:7071/api/rsvp/${this.state.rsvpCode.value}`, {
+            const res = await fetch(` /api/rsvp/${this.state.rsvpCode.value}`, {
                 method: "POST",
                 headers: {
                     'Access-Control-Allow-Origin': '*',
