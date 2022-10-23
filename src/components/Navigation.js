568404a84f56ad13619e143c4f878e184b981789
Tamara Rahimi <mahra.rah@gmail.com>
2022-10-23 18:28:56 +0200

    Fix small navbar bug

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -44,7 +44,9 @@ class Navigation extends Component {
     this.setState({ menuOn: false });
   }
   handleClickOutside(event) {
-    if (this.wrapperRef && !this.wrapperRef.current.contains(event.target)) {
+    console.log(this.wrapperRef)
+    console.log(this.wrapperRef.current)
+    if (this.wrapperRef && this.wrapperRef.current && !this.wrapperRef.current.contains(event.target)) {
       this.setState({ menuOn: false });
     }
   }
