a8294dfcf994a9e7228c0b64c7645d9ab8ddc17e
Tamara Rahimi <mahra.rah@gmail.com>
2022-10-23 18:53:58 +0200

    Drag end drop initial setup

--- src/components/gallery/Upload.js
+++ src/components/gallery/Upload.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect, useCallback } from "react";
 import '../../assets/css/main.css'
-import 'react-dropzone-uploader/dist/styles.css'
-// import Dropzone from 'react-dropzone-uploader'
+import LightGallery from 'lightgallery/react';
+import 'lightgallery/css/lightgallery.css';
+import 'lightgallery/css/lg-zoom.css';
+import 'lightgallery/css/lg-thumbnail.css';
+
 import { useDropzone } from 'react-dropzone'
-// import { getDroppedOrSelectedFiles, } from 'html5-file-selector';
 
 import { useTranslation } from "react-i18next";
 
@@ -16,17 +18,17 @@ function Upload() {
     const { acceptedFiles, getRootProps, getInputProps } = useDropzone({
         onDrop: acceptedFiles => {
             console.log(acceptedFiles)
-        if (acceptedFiles.length === 0) {
-            console.log(acceptedFiles)
-            setSelectedFile(undefined)
-            return
-        }
+            if (acceptedFiles.length === 0) {
+                console.log(acceptedFiles)
+                setSelectedFile(undefined)
+                return
+            }
 
-        // I've kept this example simple by using the first image instead of multiple
-        // setTheArray(oldArray => [...oldArray, newElement]);setSelectedFilesetSelectedFile
-        console.log(acceptedFiles)
-        console.log(selectedFiles)
-        setSelectedFiles(() => [...acceptedFiles])
+            // I've kept this example simple by using the first image instead of multiple
+            // setTheArray(oldArray => [...oldArray, newElement]);setSelectedFilesetSelectedFile
+            console.log(acceptedFiles)
+            console.log(selectedFiles)
+            setSelectedFiles(() => [...acceptedFiles])
         }
     });
 
@@ -55,111 +57,38 @@ function Upload() {
         }
     }, [selectedFiles])
 
-    // const onSelectFile = e => {
 
-    //     console.log(e.target.files)
-    //     if (e.target.files.length === 0) {
-    //         console.log(e.target.files)
-    //         setSelectedFile(undefined)
-    //         return
-    //     }
+    const deleteFiles = () => {
+        console.log(previews)
+        setPreviews([])
+        setSelectedFiles([])
+        setFiles([])
+        console.log(previews)
+    }
 
-    //     // I've kept this example simple by using the first image instead of multiple
-    //     // setTheArray(oldArray => [...oldArray, newElement]);setSelectedFilesetSelectedFile
-    //     console.log(e.target.files)
-    //     console.log(selectedFiles)
-    //     setSelectedFiles(() => [...e.target.files])
-
-    // }
-
-    // const onDrop = useCallback(acceptedFiles => {
-    //     // Do something with the files
-    //     console.log(acceptedFiles)
-    //     if (acceptedFiles.length === 0) {
-    //         console.log(acceptedFiles)
-    //         setSelectedFile(undefined)
-    //         return
-    //     }
-
-    //     // I've kept this example simple by using the first image instead of multiple
-    //     // setTheArray(oldArray => [...oldArray, newElement]);setSelectedFilesetSelectedFile
-    //     console.log(acceptedFiles)
-    //     console.log(selectedFiles)
-    //     setSelectedFiles(() => [...acceptedFiles])
-    // }, [])
 
     return (
-        <div>
-            <div style={{ borderColor: "green" }}>
-
-                {(previews || []).map(url => (
-                    <img style={{ width: "100px", hight: "100px" }} key={url} src={url} alt="..." />
-                ))}
-            </div>
-            <div {...getRootProps({ className: 'dropzone' })}>
-                <input {...getInputProps()} />
-                <p>Drag 'n' drop some files here, or click to select files</p>
-            </div>
-            {/* <Dropzone onDrop={onDrop} accept={"image/*"} /> */}
-            {/* <input type='file' multiple onChange={onSelectFile} accept={"image/*"} /> */}
-        </div>
+        <section className="post">
+            <h2>Upload own images</h2><div class="box">
+                <p>Do you have immages you would like to share with us? No problem! Just send to us bellow </p>
+                <div>
+                    <LightGallery
+                        speed={500}
+                    >
+                        {(previews || []).map((url, i) => (
+                            <img style={{ width: "100px", hight: "100px" }} key={i} src={url} alt="..." />
+                        ))}
+                    </LightGallery>
+                </div>
+                <div {...getRootProps({ className: 'dropzone' })} style={{ background: "grey", height: "200px" }}>
+                    <input {...getInputProps()} />
+                    <p style={{ textAlign: "center" }}> Drag 'n' drop some files here, or click to select files</p>
+                </div>
+                <button onClick={() => deleteFiles()}>Delete</button>
+            </div></section >
     )
 }
 
 
-
-// function Upload() {
-//     const fileParams = ({ meta }) => {
-//         return { url: 'https://httpbin.org/post' }
-//     }
-//     const onFileChange = ({ meta, file }, status) => { 
-//         console.log(status, meta, file) 
-//     }
-//     const onSubmit = (files, allFiles) => {
-//         allFiles.forEach(f => f.remove())
-//     }
-//     const getFilesFromEvent = e => {
-//         return new Promise(resolve => {
-//             getDroppedOrSelectedFiles(e).then(chosenFiles => {
-//                 resolve(chosenFiles.map(f => f.fileObject))
-//             })
-//         })
-//     }
-//     const selectFileInput = ({ accept, onFiles, files, getFilesFromEvent }) => {
-//         const textMsg = files.length > 0 ? 'Upload Again' : 'Select Files'
-//         return (
-//             <label className="btn btn-danger mt-4" style={{color:"white"}}>
-//                 {textMsg}
-//                 <input
-//                     style={{ display: 'none' }}
-//                     type="file"
-//                     accept={accept}
-//                     multiple
-//                     onChange={e => {
-//                         getFilesFromEvent(e).then(chosenFiles => {
-//                             onFiles(chosenFiles)
-//                         })
-//                     }}
-//                 />
-//             </label>
-//         )
-//     }
-//     return (
-//         <Dropzone
-//             onSubmit={onSubmit}
-//             onChangeStatus={onFileChange}
-//             InputComponent={selectFileInput}
-//             getUploadParams={fileParams}
-//             getFilesFromEvent={getFilesFromEvent}
-//             accept="image/*,video/*"
-//             maxFiles={5}
-//             inputContent="Drop A File"
-//             styles={{
-//                 dropzone: { width: 600, height: 400 },
-//                 dropzoneActive: { borderColor: 'green' },
-//             }}            
-//         />
-//     );
-// }
 // https://www.positronx.io/react-single-and-multiple-images-upload-preview/
 export default Upload;
