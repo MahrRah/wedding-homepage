c38ff5e984a1fdf9389e8ffc7d04125941fca859
Tamara Rahimi <mahra.rah@gmail.com>
2022-10-23 15:40:52 +0200

    Add weddingparty

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -3,13 +3,15 @@ import React from 'react';
 import '../../assets/css/main.css'
 import { useTranslation } from "react-i18next";
 import i18n from '../../i18n.js';
+import { Grid } from 'react-loader-spinner';
 function Contact() {
   const { t } = useTranslation(["common"]);
-  const Person = ({ side, align }) => {
+  const Person = ({ side, align, name, text }) => {
     return (
       <>
         <span className={side} style={{ width: 200 }}><img src={require("../../images/person-placeholder.jpg")} alt="" /></span>
-        <p style={{ height: 200, textAlign: align }}>Hi my name is Vale</p>
+        <h3 style={{ textAlign: align }}>{name}</h3>
+        <p style={{ height: 200, textAlign: align }}>{text}</p>
       </>);
   };
 
@@ -19,11 +21,20 @@ function Contact() {
         <header className="major">
           <h1>{t("common:contacts")}</h1>
         </header>
-        <Person side={"image left"} align={'left'} />
-        <Person side={"image right"} align={'right'} />
-      </section>
+        <h2>Our Best People</h2>
+        <Person side="image left" align='left' name="Christian" text="asds" />
+        <Person side="image right" align='right' name="Priska" text="asds" />
+        <hr />
 
-    </div>
+<h2>The Groomsman</h2>
+        <Person side="image left" align='left' name="Moritz" text="asds" />
+        <Person side="image left" align='left' name="TBD" text="asds" />
+        
+        <h2 style={{textAlign: "right"}}>The Bridesmaids</h2>
+        <Person side="image right" align='right' name="Vanessa" text="asds" />
+        <Person side="image right" align='right' name="Julia" text="asds" />
+      </section >
+</div>
   );
 }
 
